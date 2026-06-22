5e9fed05adb14fda9ada86ba4cba14fd87ebc8ab
cat-coder-watcher <tadeusouza004@gmail.com>
2026-06-21 21:08:32 -0300

    Terceiro exercicio/Segunda tarefa resposta

---
+++ Potenciacao.py
@@ -0,0 +1,9 @@
+# Pede os dados ao usuário
+base = int(input("Digite a base: "))
+expoente = int(input("Digite o expoente: "))
+
+# Calcula a potência
+resultado = base**expoente
+
+# Mostra o resultado
+print(f"{base} elevado a {expoente} é igual a {resultado}")
