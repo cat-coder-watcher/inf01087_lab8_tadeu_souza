0e96a2f9078c73adb6159670a06662b6e81ebde4
Tadeu Souza <tadeusouza004@gmail.com>
2026-06-21 21:18:24 -0300

    Commitado antes da hora 2

--- Potenciacao.py
+++
@@ -1,9 +0,0 @@
-# Pede os dados ao usuário
-base = int(input("Digite a base: "))
-expoente = int(input("Digite o expoente: "))
-
-# Calcula a potência
-resultado = base**expoente
-
-# Mostra o resultado
-print(f"{base} elevado a {expoente} é igual a {resultado}")
